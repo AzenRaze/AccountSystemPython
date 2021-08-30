6c16fb3b5a3134c094df7430374403b0720c49f5
AzenRaze <89753501+AzenRaze@users.noreply.github.com>
2021-08-30 21:30:11 +0100

    Add files via upload

--- AccountSystem.py
+++ AccountSystem.py
@@ -1,16 +1,16 @@
 
 Accorlog = input("Press 1 to make an account,Press 2 to login to an account: ")
+lmao = 0
 
 if Accorlog == "1":
-    name = input("Name:")
-    psw = input("Password:")
+   name = input("Name:")
+   psw = input("Password:")
     
-    with open("accounts.txt","a") as f:
-        f.write(f"{name},{psw}\n")
-        print("Account added")
-    
+   with open("accounts.txt","a") as f:
+        f.write(f"{name},\n{psw}\n\n")
+        print("Account added, if login does not work it has already been taken")
+
 if Accorlog == "2":
-    correct =0
     
     name = input("Name:")
     psw = input("Password:")
@@ -19,16 +19,18 @@ if Accorlog == "2":
         accounts = f.readlines()
         
         
+        
     for acc in accounts:
         name_txt = acc.split(",")[0]
         psw_txt = acc.split(",")[1]
         
         psw_txt = psw.replace("\n", "")
-        
 
-            
     if name_txt == name  and psw_txt == psw:
         print("logged in")
     else:
         print("nope")
-      
+        
+quit()
+        
+
